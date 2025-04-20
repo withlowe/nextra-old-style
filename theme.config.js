2d10171c84735a7c4de245d2b05126777ff773fe
With Lowe <heynick@duck.com>
2025-04-20 16:48:58 +0100

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -5,7 +5,7 @@ export default {
   search: true,
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold md:inline">Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         The Next Site Builder
       </span>
